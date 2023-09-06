cea818f545d821e7220fb04f529b94650ad0a3da
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2023-09-06 20:08:42 -0300

    project init

---
+++ backend/src/main/java/br/com/gabrielferreira/apievento/ApiEventoApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.apievento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEventoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEventoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gabrielferreira/apievento/ApiEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.apievento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
