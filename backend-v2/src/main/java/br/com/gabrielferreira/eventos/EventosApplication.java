702a843d82c2d7d90f77d117011d48f7a38e8ddc
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2024-01-28 21:19:34 -0300

    Criação script perfil e criação entidade perfil

--- backend-v2/src/main/java/br/com/gabrielferreira/eventos/EventosApplication.java
+++ backend-v2/src/main/java/br/com/gabrielferreira/eventos/EventosApplication.java
@@ -1,4 +1,4 @@
-package br.com.gabrielferreira.eventos.eventos;
+package br.com.gabrielferreira.eventos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ backend-v2/src/main/java/br/com/gabrielferreira/eventos/domain/model/Perfil.java
@@ -0,0 +1,32 @@
+package br.com.gabrielferreira.eventos.domain.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "TB_PERFIL")
+public class Perfil implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 124441226725634777L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(name = "DESCRICAO", nullable = false)
+    private String descricao;
+
+    @Column(name = "AUTORIEDADE", nullable = false, unique = true)
+    private String autoriedade;
+}
