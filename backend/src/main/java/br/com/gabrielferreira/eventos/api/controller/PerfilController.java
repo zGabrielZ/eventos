e0a1ab6d60783508dfa9f1b7813d8664ed377cf4
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2024-02-14 17:57:42 -0300

    Doc de perfil controller

--- backend/src/main/java/br/com/gabrielferreira/eventos/api/controller/PerfilController.java
+++ backend/src/main/java/br/com/gabrielferreira/eventos/api/controller/PerfilController.java
@@ -4,6 +4,12 @@ import br.com.gabrielferreira.eventos.api.mapper.PerfilMapper;
 import br.com.gabrielferreira.eventos.api.model.PerfilModel;
 import br.com.gabrielferreira.eventos.domain.model.Perfil;
 import br.com.gabrielferreira.eventos.domain.service.PerfilService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Tag(name = "Perfil Controller", description = "Endpoints para realizar consulta de perfil")
 @RestController
 @RequestMapping("/perfis")
 @RequiredArgsConstructor
@@ -22,6 +29,12 @@ public class PerfilController {
 
     private final PerfilMapper perfilMapper;
 
+    @Operation(summary = "Buscar perfis")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Perfis encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PerfilModel.class)) })
+    })
     @GetMapping
     public ResponseEntity<List<PerfilModel>> buscarPerfis(){
         List<Perfil> perfis = perfilService.buscarPerfis();
@@ -30,6 +43,14 @@ public class PerfilController {
         return ResponseEntity.ok().body(perfilModels);
     }
 
+    @Operation(summary = "Buscar perfil por id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Perfil encontrado",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = PerfilModel.class)) }),
+            @ApiResponse(responseCode = "404", description = "Perfil não encontrado",
+                    content = @Content)
+    })
     @GetMapping("/{id}")
     public ResponseEntity<PerfilModel> buscarPerfilPorId(@PathVariable Long id){
         Perfil perfil = perfilService.buscarPerfilPorId(id);
--- backend/src/main/java/br/com/gabrielferreira/eventos/api/model/PerfilModel.java
+++ backend/src/main/java/br/com/gabrielferreira/eventos/api/model/PerfilModel.java
@@ -1,5 +1,6 @@
 package br.com.gabrielferreira.eventos.api.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,9 +18,12 @@ public class PerfilModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 150854081687212908L;
 
+    @Schema(description = "Id do perfil", example = "1")
     private Long id;
 
+    @Schema(description = "Descrição do perfil", example = "Cliente")
     private String descricao;
 
+    @Schema(description = "Autoriedade do perfil, tipo do perfil", example = "ROLE_CLIENT")
     private String autoriedade;
 }
